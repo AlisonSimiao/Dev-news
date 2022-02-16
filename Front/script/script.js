1ff556d4e0a39c2f55c321a9ef7574636bbb4d8f
ALISON <superalison>
2022-02-16 19:09:12 -0300

    add input, delete

--- Front/script/script.js
+++ Front/script/script.js
@@ -1,24 +1,46 @@
-let $corpo = document.getElementById("corpo");
+const  $corpo = document.getElementById("corpo"),
+        input = document.querySelector("input");
 
+input.addEventListener("change", (e)=>{
+    pegaNoticias(input.value);
+});
 
-async function pegaNoticias() {
-    const resposta = await fetch("http://localhost:3000/api/noticias");
-    const n = await resposta.json();
-    return n;
-}
-
-
-pegaNoticias().then( (res)=>{
-    console.log(res);
-    for( noticia of res ){
-        $corpo.innerHTML += `<div class='noticia'>
+async function pegaNoticias(search="") {
+    const resposta = await fetch("http://localhost:3000/api/noticias?search="+search);
+    const noticias = await resposta.json();
+    
+    $corpo.innerHTML= "";
+    for( noticia of noticias ){
+        $corpo.innerHTML += `<div class='noticia' >
                                 <div class='titulo'>
                                     <h1>${noticia.title ?? noticia.titulo }</h1>
                                     <hr width='30%'>
                                 </div>
                                 <span>${noticia.intro}</span>
                                 <p class='cat'>${noticia.type ?? noticia.categoria}</p>
+                                <div class='close' id='${ noticia.id }'>X</div>
                             </div>`
     } 
+    const close = document.querySelectorAll(".close");
+    close.forEach((x)=>{
+        x.addEventListener("click", async (e)=>{
+            const resposta = await fetch("http://localhost:3000/api/noticia/"+x.id, 
+                                        {
+                                            method: 'DELETE',
+                                        }
+                                        );
+        
+            const noticias = await resposta.json();
+            console.log(resposta,noticias);
+            pegaNoticias();
+        });
+    })
     
-} );
+    
+} 
+
+window.onload = ()=>{
+    pegaNoticias();
+}
+
+
